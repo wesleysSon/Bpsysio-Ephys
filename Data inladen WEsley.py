75149547189c031ff285bb92898a8401713e0000
Wesley <wesleysontowirono@gmail.com>
2024-04-22 23:18:23 +0200

    Data ingeladen met pandas

--- Data inladen WEsley.py
+++ Data inladen WEsley.py
@@ -1,6 +1,11 @@
-#Generiek pad maken hier naartoe:
-    #1. Maak een map binnen de repository genaamd data
-    #2. Zet hierin het meegegeven excel bestand met de ephys data
-path = "C:\Users\wesle\Downloads\01. Master_Latest data_Control clones_LP.xlsx"
+#imports
+import pandas as pd
 
-#Laad hier de data in:
+#Loading dataset into the workflow. 
+    #`data` variable contains the whole dataset. 
+    #`data_extracted_features` contains certain from the whole dataset.
+
+data = pd.read_excel("data/01. Master_Latest data_Control clones_LP.xlsx", engine='openpyxl')
+data_extracted_features = pd.read_excel("data/01. Master_Latest data_Control clones_LP.xlsx", usecols=['Batch number', 'Genotype Neuron', 'Div calculated', 'Culture treatment', 'Capacitance', 'Input Resistance', 'Resting membrane potential ', 'Maximum firing ', 'Calculated input resistance', 'Rheobase'])
+data_extracted_features.head()
+print(data_extracted_features)
