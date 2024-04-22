e8cc65fd0c890fb84d13e3251cb98fa855379135
Wesley <wesleysontowirono@gmail.com>
2024-04-22 21:42:16 +0200

    pad variabele aangemaakt

---
+++ Data inladen WEsley.py
@@ -0,0 +1,6 @@
+#Generiek pad maken hier naartoe:
+    #1. Maak een map binnen de repository genaamd data
+    #2. Zet hierin het meegegeven excel bestand met de ephys data
+path = "C:\Users\wesle\Downloads\01. Master_Latest data_Control clones_LP.xlsx"
+
+#Laad hier de data in:
