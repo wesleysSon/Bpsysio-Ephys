6428670ff9e8cfd57658b21547fcfae9ec30690c
Wesley <wesleysontowirono@gmail.com>
2024-05-13 00:41:39 +0200

    Variabele data aangepast, waarbij nu ook de EPSC freq wordt meegenomen

--- Data inladen WEsley.py
+++ Data inladen WEsley.py
@@ -2,19 +2,24 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
+# import sklearn
 
 #Loading dataset into the workflow. 
     #`data` variable contains the whole dataset. 
     #`data_extracted_features` contains certain from the whole dataset.
 
 data = pd.read_excel("data/01. Master_Latest data_Control clones_LP.xlsx", engine='openpyxl')
-data_extracted_features = pd.read_excel("data/01. Master_Latest data_Control clones_LP.xlsx", usecols=['Batch number', 'Genotype Neuron', 'Div calculated', 'Culture treatment', 'Capacitance', 'Input Resistance', 'Resting membrane potential ', 'Maximum firing ', 'Calculated input resistance', 'Rheobase'])
+data_extracted_features = pd.read_excel("data/01. Master_Latest data_Control clones_LP.xlsx", usecols=['Batch number', 'Genotype Neuron', 'Div calculated', 'Culture treatment', 'Capacitance', 'Input Resistance', 'Resting membrane potential ', 'Maximum firing ', 'Calculated input resistance', 'Rheobase', 'EPSC freq'])
 #data_extracted_features.head()
-data_extracted_features_nona = data_extracted_features.dropna()
-#print(data_extracted_features.dropna())
+#data_extracted_features_nona = data_extracted_features.dropna()
+print(data_extracted_features)
 
-sns.violinplot(data=data_extracted_features_nona, x="Batch number", y="Div calculated")
-plt.xlabel("Batch number")
-plt.ylabel("Div calculated")
-plt.title("Ephys no na violinplot")
-plt.show()
+#sns.violinplot(data=data_extracted_features_nona)
+# sns.violinplot(data=data_extracted_features, x="Batch number", y="Div calculated")
+# plt.xlabel("Batch number")
+# plt.ylabel("Div calculated")
+# plt.title("Ephys no na violinplot")
+# plt.show()
+
+
