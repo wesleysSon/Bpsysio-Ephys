2501826e6ee06580fa7c7c90b610fc13b5bf3115
Wesley <wesleysontowirono@gmail.com>
2024-05-14 00:29:08 +0200

    Jupyter notebook met MICE imputed values. Ook violinplotsss voor vergelijking voor en na imputatie. Alleen nog ranges zien te imputeren

--- Data inladen WEsley.py
+++ Data inladen WEsley.py
@@ -21,13 +21,17 @@ data_extracted_features = pd.read_excel("data/01. Master_Latest data_Control clo
 # plt.title("Ephys no na violinplot")
 # plt.show()
 
+#voor en na preproceesing distributie van de data bekijken. 
+# Plot data (per feature) distributie voor en na om te kijken hoe je impute method invloed heeft op de data.  
+# glm 
+
 print("Preprocessing data...")
-#print(data_extracted_features)  
-data_extracted_features = data_extracted_features.dropna()
-#print(data_extracted_features)  
+print(data_extracted_features)  
+#data_extracted_features = data_extracted_features.dropna()
+print(data_extracted_features)  
 range_filter = data_extracted_features[data_extracted_features['EPSC freq'].str.contains('>') == True]     
 data_extracted_features = data_extracted_features.drop(range_filter.index)
-#print(data_extracted_features)  
+print(data_extracted_features)  
 
 print("Splitting data...")
 X_train, X_test= train_test_split(data_extracted_features, test_size=0.2, random_state=51)
@@ -35,16 +39,22 @@ X_train, X_test= train_test_split(data_extracted_features, test_size=0.2, random
 #print(X_test)   
 
 print("Loading violinplot...")
+# kwartielen erin plotten
 # sns.violinplot(data=data_extracted_features, x="Batch number", y="Genotype Neuron")
 sns.violinplot(data=data_extracted_features, x="Batch number", y="Div calculated")
 plt.xlabel("Batch number")
 plt.ylabel("Div calculated")
 plt.title("Ephys no na violinplot")
-plt.show()
+# plt.show()
 
-# X_train, X_test= train_test_split(data_extracted_features, test_size=0.2, random_state=42)
-# print(X_train)
-# print(X_test)   
 
+# print("Loading scatterplot...")
+# g = sns.FacetGrid(data_extracted_features, col="Batch number", hue="Genotype Neuron")
+# g.map(sns.scatterplot, "", "Div calculated", alpha=.7)
+# g.add_legend()
+# plt.show()    
+
+# sns.scatterplot(data=data_extracted_features, x="Batch number", y="Div calculated", hue="Genotype Neuron")           
+# plt.show()  
 # pca = PCA(n_components=2)
 # pca.fit(data_extracted_features)
